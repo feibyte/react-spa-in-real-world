eb340f7859d88229868079d684b76ce53fa79479
fedeoo <fedeoo.zf@gmail.com>
2019-01-25 17:15:57 +1100

    Set prettier

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  presets: [['@babel/preset-env', {
-    useBuiltIns: 'entry',
-  }], '@babel/preset-flow', '@babel/preset-react'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry',
+      },
+    ],
+    '@babel/preset-flow',
+    '@babel/preset-react',
+  ],
 };
